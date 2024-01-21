7743e75661fc64874c48e94e07c4613c83cbadcf
mouchang89 <mou.chang@atlasschool.com>
2024-01-21 01:21:58 +0000

    hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,2 @@
 #!/bin/bash
-#incude <stdio.h>
-/**
- * main - Prints "Programming is like building a mutiligua puzzle," followed by a new line.
- * Return: Always 0
- * /
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
+puts("\"Programming is like building a multilingual puzzle")
