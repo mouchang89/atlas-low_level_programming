5e0a9c45544d4d336c141d8d183c30479b474ef5
mouchang89 <mou.chang@atlasschool@gmail.com>
2024-01-22 02:57:12 +0000

    last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -6,13 +6,14 @@
  * main - Prints the last digit of a randomly generated number
  * and whether the number is greater than 5, 0, or less than 6 and not 0.
  *
- * Return: Always successful.
+ * Return: Always 0.
  */
 
 int main(void)
 
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
@@ -21,15 +22,14 @@ int main(void)
 		printf("last digit of %d is %d and is greater than 5\n",
 			n, n % 10);
 	}
-	else if ((n % 10) == 0)
-	{
-		printf("last digit of %d is %d and is 0\n",
-			n, n % 10);
-	}
 	else if ((n % 10) < 6 && (n % 10) != 0)
 	{
 		printf("last digit of %d is %d and is less than 6 and not  0\n",
 				n, n % 10);
+	}
+	else
+	{
+		printf("last digit of %d is %d and is 0\n", n, n% 10);
 	}
 	return (0);
 
