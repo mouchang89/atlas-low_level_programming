a7ae3f7c3955e6243ed4536a9b841b7812a45bd1
mouchang89 <mou.chang@atlasschool@gmail.com>
2024-01-22 03:37:05 +0000

    alphABET

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -5,20 +5,17 @@
  *
  * Return: Always 0.
  */
-
 int main(void)
-
 {
-
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-	putchar(letter);
+		putchar(letter);
 
 	for (letter = 'A'; letter <= 'Z'; letter++)
-	putchar(letter);
+		putchar(letter);
+
 	putchar('\n');
 
 	return (0);
-
 }
