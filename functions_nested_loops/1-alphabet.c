568d304a64b104e1543d3d79e917368c1d950779
mouchang89 <mou.chang@atlasschool@gmail.com>
2024-01-22 06:39:02 +0000

    task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Prints the alphabet in lowercase using only _putchar.
+ * 
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char i = 'a';
+
+	while (i <= 'z'
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n')
+}
