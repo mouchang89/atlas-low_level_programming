84392b39e74ca26f3c936ad59c51b7acf86762d6
mouchang89 <mou.chang@atlasschool@gmail.com>
2024-01-22 03:12:21 +0000

    last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -16,21 +16,20 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lastd = n % 10;
 
-	if (lastd > 5)
+	if ((n % 10) > 5)
 	{
 		printf("last digit of %d is %d and is greater than 5\n",
-			n, lastd);
+			n, (n % 10));
 	}
-	else if (lastd == 0)
+	else if ((n % 10) == 0)
 	{
-		printf("last digit of %d is %d and is 0\n", n, lastd);
+		printf("last digit of %d is %d and is 0\n", n, (n % 10));
 	}
-	else if (lastd < 6 && lastd != 0)
+	else if ((n % 10) < 6 && (n % 10) != 0)
 	{
 		printf("last digit of %d is %d and is less than 6 and not  0\n",
-		n, lastd);
+		n, n % 10);
 	}
 	return (0);
 
