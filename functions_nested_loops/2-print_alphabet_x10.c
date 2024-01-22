ef8a82107e107e1e1c42fdebf1728848a46103d4
mouchang89 <mou.chang@atlasschool.com>
2024-01-22 20:20:20 +0000

    10 x alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Prints alphabet 10 times in lower case.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int num = 0;
+	char i;
+
+	while (num++ <= 9)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+		_putchar('\n')
+	}
+}
