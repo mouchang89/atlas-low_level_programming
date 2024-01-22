b92eca37085d9e6da31121d2fb222e47b61c4cc8
mouchang89 <mou.chang@atlasschool.com>
2024-01-22 21:05:50 +0000

    task 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Computes the absolute value of an integer.
+ *
+ * Returns: Always 0.
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+		return (i);
+	else
+		return (-i);
+}
