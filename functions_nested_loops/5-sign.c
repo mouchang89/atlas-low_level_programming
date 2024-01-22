725c6b060a0e4d52c0c9d3b44a5aaa660647e4ce
mouchang89 <mou.chang@atlasschool.com>
2024-01-22 21:00:30 +0000

    sign

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - Prints the sign of a number.
+ *
+ * Returns 1 and prints + if n is greater than 0.
+ * Returns 0 and prints 0 if n is 0.
+ * Returns -1 and prints - if n is less than 0.
+ */
+int print_sign(int n);
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
