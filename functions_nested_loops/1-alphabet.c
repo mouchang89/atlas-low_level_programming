d405579aa35d86f31f884f59d25dce2a977c70f6
mouchang89 <mou.chang@atlasschool.com>
2024-01-22 19:45:02 +0000

    task 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -5,13 +5,14 @@
  * 
  * Return: Always 0.
  */
-void alphabet(void)
+void print_alphabet(void)
 {
 	char i;
 
 	for (i = 'a'; i <= 'z'; i++)
 	{
-	_putchar(i);
+		_putchar(i);
 	}
+	
 	_putchar('\n');
 }
