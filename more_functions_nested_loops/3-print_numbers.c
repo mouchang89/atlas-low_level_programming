15924a97ba4c04ac850387671a81d4841bc5b88d
mouchang89 <mou.chang@atasschol.com>
2024-01-27 22:02:07 +0000

    printing 0-9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints numbers 0 to 9 followed by new line.
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int num;
+	
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar(num + '0');
+	}
+	_putchar('\n');
+}
