4de06c615a1ad1d6a0a1dfbc007a0aa570eac434
mouchang89 <mou.chang@atasschol.com>
2024-01-27 23:25:23 +0000

    removed ascii 48 to 0

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -9,9 +9,9 @@ void more_numbers(void)
 {
 	int num1, num2;
 
-	for (num1 = 48; num1 < 10; num1++)
+	for (num1 = 0; num1 < 10; num1++)
 	{
-		for (num2 = 48; num2 <= 14; num2++);
+		for (num2 = 0; num2 <= 14; num2++);
 		
 		if (num2 > 9)
 		
