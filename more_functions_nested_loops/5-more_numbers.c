0711a35328b0e27470a08d6c2e2ad019260a7378
mouchang89 <mou.chang@atasschol.com>
2024-01-27 23:24:03 +0000

    removed {} for if statement

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -9,12 +9,12 @@ void more_numbers(void)
 {
 	int num1, num2;
 
-	for (num1 = 0; num1 < 10; num1++)
+	for (num1 = 48; num1 < 10; num1++)
 	{
-		for (num2 = 0; num2 <= 14; num2++);
-		{
-			if (num2 > 9)
-		}
+		for (num2 = 48; num2 <= 14; num2++);
+		
+		if (num2 > 9)
+		
 		_putchar((num2 / 10) + '0');
 	}
 	_putchar((num2 % 10) + '0');
