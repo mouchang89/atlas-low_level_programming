2fc98022f59ca5653d8dc8fe1d4b9787a719f0b3
mouchang89 <mou.chang@atlasschool.com>
2024-01-28 00:22:08 +0000

    moved putchar around

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -9,13 +9,13 @@ void more_numbers(void)
 {
 	int num1, num2;
 
-	for (num1 = 0; num1 < 10; num1++)
+	for (num1 = 0; num1 <= 14; num1++)
 	{
-		for (num2 = 0; num2 <= 14; num2++)
+		for (num2 = 0; num2 < 10; num2++)
 		{
-			_putchar(num2 + '0');
-			_putchar('\n');
+			_putchar(num2 + 0\n');
 		}
 	}
+	_putchar(num1 + 0);
 	_putchar('\n');
 }
