5d9ed639a2ad0d3c5ec7f6d32ee0b5f3e64753f4
mouchang89 <mou.chang@atlasschool.com>
2024-01-28 03:12:02 +0000

    changed else if statement to if

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -19,10 +19,10 @@ int main(void)
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuz");
 
-		else if (i % 3 == 0)
+		if (i % 3 == 0)
 			printf("Fizz");
 
-		else if (i % 5 == 0);
+		if (i % 5 == 0);
 			printf("Buzz");
 
 		else
