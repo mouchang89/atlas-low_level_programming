c628926b7cf7695051be5bc14e3df68e4e8b5cc0
mouchang89 <mou.chang@atlasschool.com>
2024-01-28 00:09:15 +0000

    fixed function to print 0-14 10 times

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -11,12 +11,11 @@ void more_numbers(void)
 
 	for (num1 = 0; num1 < 10; num1++)
 	{
-		for (num2 = 0; num2 <= 14; num2++);
-		
-		if (num2 > 9)
-		
-		_putchar((num2 / 10) + '0');
-		_putchar((num2 % 10) + '0');
+		for (num2 = 0; num2 <= 14; num2++)
+		{
+			_putchar(num2 + '0');
+		}
 	}
+	_putchar((num1 + '0');
 	_putchar('\n');
 }
