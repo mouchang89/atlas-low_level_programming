048a9e2ce23f9c6aab9c0c74f981967333ffc774
mouchang89 <mou.chang@atlasschool.com>
2024-01-28 02:52:36 +0000

    prints square using #

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_square - Prints a square followed by new line.
+ * @size: Size of the square.
+ *
+ * Return: Always 0.
+ */
+void print_square(int size);
+{
+	int a, b;
+
+	b = 0;
+
+	if (size < 1)
+	{
+		_putchar('\n')
+	}
+
+	while (b < size)
+	{
+		a = 0;
+
+		while (a < size)
+		{
+			_putchar('#');
+			a++;
+		}
+		_putchar('\n');
+		b++;
+	}
+}
