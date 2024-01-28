8369bc0227a53ef0c02ee3f1c2e33e69a6072543
mouchang89 <mou.chang@atlasschool.com>
2024-01-28 02:43:28 +0000

    draws diagonal line on terminal

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -9,4 +9,24 @@
  */
 void print_diagonal(int n)
 {
+	int a, b;
 
+	a = 0;
+
+	while (n > 0)
+	{
+		b = c;
+
+		while (b > 0)
+		{
+			_putchar(' ');
+			b--;
+		}
+		_putchar('\\');
+		_putchar('\n');
+		a++;
+		n--;
+	}
+	if (a < 1)
+		_putchar('\n');
+}
