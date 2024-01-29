bff4ac84ad2decc62e7751ffb6e557dc838f1062
mouchang89 <mou.chang@atlasschool.com>
2024-01-29 22:31:45 +0000

    comment line too long

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * reset_to_98 - Takes pointer to int as parameter and updates value it points to to 98.
+ * reset_to_98 - Takes pointer to int as parameter and updates
+ * value it points to to 98.
  * @n: Value
  *
  * Return: Always 0.
