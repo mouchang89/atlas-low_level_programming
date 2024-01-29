a56ed98026b483caf968965623ec978d7c6032db
mouchang89 <mou.chang@atlasschool.com>
2024-01-29 22:52:20 +0000

    finding length of a string

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns length of a string.
+ * @s: character variable.
+ *
+ * Return: Length of string.
+ */
+int _strlen(char *s)
+{
+	strlen(*s);
+
+	return (*s);
+}
