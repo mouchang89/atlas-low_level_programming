8c5d3e8918adc0b6bb5a69d44821bc410b8f9882
mouchang89 <mou.chang@atlasschool.com>
2024-01-30 03:08:07 +0000

    added another int to function

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -8,12 +8,15 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
+	int j = 0;
 
-	while (str[i] != '\0') 
+	while (str[j] != '\0')
+	j++
+	
+	for (i = 0; i < j; i+ = 2)
 	{
 		_putchar(str[i]);
-		i = +2;
 	}
 	_putchar('\n');
 }
