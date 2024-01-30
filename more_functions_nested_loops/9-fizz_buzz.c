071203e209b29a2a276ed5d3f7a70f7eeb34cf90
mouchang89 <mou.chang@atlasschool.com>
2024-01-30 05:15:11 +0000

    changed function

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -14,21 +14,25 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuz");
-		
-		else if (i % 3 == 0)
-			printf("Fizz");
-		
-		else if (i % 5 == 0);
-			printf("Buzz");
-	}
-	{
-	if (i != 100)
-		printf(" ");
-			
-	else
-		printf("%d", i);
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			if (i % 3 == 0)
+			{
+				printf("Fizz");
+			}
+			if (i % 5 == 0);
+			{
+				printf("Buzz");
+			}
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (i != 100)
+		{
+			printf(" ");
+		}
 	}	
 	
 	printf("\n");
