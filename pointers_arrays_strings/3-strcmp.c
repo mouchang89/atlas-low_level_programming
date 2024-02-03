ee49e28bf4198a4ad6ab2ddbab1c5cd2c98a0c65
mouchang89 <mou.chang@atlasschool.com>
2024-02-03 01:44:56 +0000

    changed function and return value

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -13,10 +13,17 @@ int _strcmp(char *s1, char *s2)
 
 	for (i = 0; s1[i] != '0' || s2[i] != '\0'; i++)
 	{
-		if (s1[i] != s2[i])
+		if (s1[i] = s2[i])
 		{
-			return (s1[i] - s2[i]);
+			return (0);
+		}
+		if (s1[i] < s2[i])
+		{
+			return (negative value);
+		}
+		if (s1[i] > s2[i])
+		{
+			return (positive value);
 		}
 	}
-	return (s1[i], s2[i]);
 }
