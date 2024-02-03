5b4a024bb7a0164f252e51c77a387c6dd49323fd
mouchang89 <mou.chang@atlasschool.com>
2024-02-03 01:04:03 +0000

    changed condition of and to or

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -12,8 +12,9 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int a = 0;
 
-	while (dest[a] = src[a] && a < n)
+	while (dest[a] != '\0' || a < n)
 	{
+		dest[a] = src[a];
 		a++;
 	}
 	dest[a] = '\0';
