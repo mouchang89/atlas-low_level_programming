423b7f46c16f740b16bc84d58b9de23b9412a773
mouchang89 <mou.chang@atlasschool.com>
2024-02-06 00:23:28 +0000

    changed char variable

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -3,24 +3,24 @@
 /**
  * _strchr - Locates a character in a string.
  * @c: Pointer to string.
- * @c: character.
+ * @b: character.
  *
  * Return: Pointer to first occurence of character c in string s
  * or NULL if character not found.
  */
-char *_strchr(char *c, char c )
+char *_strchr(char *c, char b )
 {
 	while (c[0] != '\0')
 	{
-		if (c[0] == c)
+		if (c[0] == b)
 		{
 			return (c);
 		}
-		else if (c[1] == c)
+		else if (c[1] == b)
 		{
 			return (c + 1);
 		}
 		c++;
 	}
-	return (c + 1);
+	return ('\0');
 }
