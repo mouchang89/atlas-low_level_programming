59d9a40dfdb886a18bc3f0254b067cead1f3f512
mouchang89 <mou.chang@atlasschool.com>
2024-02-06 00:20:50 +0000

    fixed comment variable

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -2,25 +2,25 @@
 
 /**
  * _strchr - Locates a character in a string.
- * @s: Pointer to string.
+ * @c: Pointer to string.
  * @c: character.
  *
  * Return: Pointer to first occurence of character c in string s
  * or NULL if character not found.
  */
-char *_strchr(char *c, char c)
+char *_strchr(char *c, char c )
 {
 	while (c[0] != '\0')
 	{
 		if (c[0] == c)
 		{
-			return (*c);
+			return (c);
 		}
 		else if (c[1] == c)
 		{
-			return (*c + 1);
+			return (c + 1);
 		}
-		*c++;
+		c++;
 	}
-	return (*c + 1);
+	return (c + 1);
 }
