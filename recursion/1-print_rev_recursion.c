af6542e1fb4d68f71149f8e657cf38c00bbed1a6
mouchang89 <mou.chang@atlasschool.com>
2024-02-12 17:31:51 +0000

    prints string in reverse

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints a string in reverse.
+ * @s: Pointer to string.
+ *
+ * Return: String in reverse.
+ */
+void _print_rev_recursion(char *s);
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
