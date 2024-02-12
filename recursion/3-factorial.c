d7d448daf3b9a9f4a2ea87336d38432cee27bf02
mouchang89 <mou.chang@atlasschool.com>
2024-02-12 17:50:13 +0000

    returns factorial of given number

---
+++ recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * factorial - Returns factorial of a given number.
+ * @n: integer.
+ *
+ * Return: -1 if error or 1 if successful.
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1)'
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n -1));
+}
