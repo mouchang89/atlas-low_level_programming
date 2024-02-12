ac89a83f9f1779739bb653a95c17b7fe453c45c4
mouchang89 <mou.chang@atlasschool.com>
2024-02-12 17:59:18 +0000

    betty spaces needed

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -9,7 +9,7 @@
  */
 int _pow_recursion(int x, int y)
 {
-	if (y < 0 )
+	if (y < 0)
 	{
 		return (-1);
 	}
@@ -17,5 +17,5 @@ int _pow_recursion(int x, int y)
 	{
 		return (1);
 	}
-	return (x * _pow_recursion(x, y -1));
+	return (x * _pow_recursion(x, y - 1));
 }
