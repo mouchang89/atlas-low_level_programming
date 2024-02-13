ae1f4dde4e7f2d0a979a60c70f7ad5304cf55c8e
mouchang89 <mou.chang@atlasschool.com>
2024-02-13 04:32:36 +0000

    Prints result of multiplication

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Print result of multiplication.
+ * @argc: Number of command line arguments.
+ * @argv: Array name.
+ *
+ * Return: 1 if error, otherwise 0.
+ */
+int main(int argc, char *argv[])
+{
+	int num1 = 0, num2 = 0;
+
+	if (argc == 3)
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		printf("%d\n", num1 * num2);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
