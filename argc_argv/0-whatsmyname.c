811cb82b674ce750d6098e31766a529d1f241bc5
mouchang89 <mou.chang@atlasschool.com>
2024-02-13 03:53:04 +0000

    Prints name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints name.
+ * @argc: Number of command line arguments.
+ * @argv: Array name.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+	{
+		return (0);
+	}
+}
