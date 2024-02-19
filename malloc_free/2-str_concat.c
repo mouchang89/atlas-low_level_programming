8a577492eb2027cf35d21bdb8ac972106762a9a0
mouchang89 <mou.chang@atlasschool.com>
2024-02-19 23:30:51 +0000

    changed function

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,39 +13,43 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *result;
-	int i, j, len, len1, len2;
+	int i = 0, j = 0, k = 0, l = 0;
 
-	if (s1 != NULL)
+	if (s1 == NULL)
 	{
-		i = 0;
-		while (s1[i++] != '\0')
-		{
-			len1++;
-		}
+		s1 = "";
 	}
-	if (s2 != NULL)
+	if (s2 == NULL)
 	{
-		i = 0;
-		while (s2[i++] != '\0')
-		{
-			len2++;
-		}
+		s2 == "";
 	}
-	len = len1 + len2;
-	result = malloc(sizeof(char) * (len + 1));
+	while (s1[i])
+	{
+		i++;
+	}
+	while s2[j]
+	{
+		j++;
+	}
+	l = i + j;
+	result = malloc((sizeof(char) * len) + 1);
 	if (result == NULL)
 	{
 		return (NULL);
 	}
-	for (i = 0; s1[i] != '\0'; i++)
+	while (k < l)
 	{
-		result[i] = s1[i];
+		if (k <= i)
+		{
+			result[k] = s1[k];
+		}
+		if (k >= i)
+		{
+			s[k] = s2[j];
+			j++;
+		}
+		k++;
 	}
-	for (j = 0; s2[j] != '\0'; i++)
-	{
-		result[i] = s2[j];
-		j++;
-	}
-	result[len] = '\0';
+	result[k] = '\0';
 	return (result);
 }
