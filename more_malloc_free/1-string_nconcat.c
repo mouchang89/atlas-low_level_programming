4388b9e25de9bd96c3ff9309e5958ed95cf20c88
mouchang89 <mou.chang@atlasschool.com>
2024-02-20 00:46:21 +0000

    linking 2 strings together in new memory

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - Concatenates 2 strings.
+ * @s1: String 1.
+ * @s2: String 2.
+ * @n: Number of bytes of s2 to link to s1.
+ *
+ * Return: Pointer to new memory of s1 followed by s2,
+ * otherwise NULL if fail.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *c;
+	int i, len = n;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	for (i = 0; s1[i]; i++)
+	{
+		len++;
+	}
+	c = malloc(sizeof(char) * (len + 1));
+	if (c == NULL)
+	{
+		return (NULL);
+	}
+	len = 0;
+	for (i = 0, s1[i]; i++)
+	{
+		c[len++] = s1[i];
+	}
+	for (i = 0; s2[i] && i < n; i++)
+	{
+		c[len++] = s2[i];
+	}
+	c[len] = '\0';
+	return (c);
+}
