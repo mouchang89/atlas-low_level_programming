c5ccc01be3f0743c3dfb65fb6312c146aaa588e8
mouchang89 <mou.chang@atlasschool.com>
2024-02-25 23:12:07 +0000

    added size to function parameters

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -15,15 +15,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || cmp == NULL)
+	if (array == NULL || size <= 0 || cmp == NULL)
 	{
-		for (i = 0; i < size; i++)
-		{
-			if (cmp(array[i]))
-			{	
-				return (i);
-			}
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+		{	
+			return (i);
 		}
-	}	
+	}
 	return (-1);
 }
