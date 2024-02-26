8feabb56c96d3da043cf3a8d6beb17fd1ef03ec0
mouchang89 <mou.chang@atlasschool.com>
2024-02-26 00:58:08 +0000

    prints numbers and added stdio.h to .h file

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,36 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - prints numbers
+ * @separator: string to be printed between numbers
+ * @n: number of integers passed to function
+ *
+ * Return: always 0
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list list;
+
+	va_start(list, n);
+
+	for (i = 0; i < n, i++)
+	{
+		if (separator != NULL)
+		{
+			printf("%d", va_arg(list, int));
+		}
+		else if (separator && i == 0)
+		{
+			printf("%d", va_arg(list, int));
+		}
+		else
+		{
+			printf("%d", separator, va_arg(list, int));
+		}
+	}
+	va_end(list);
+	printf("\n");
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -2,6 +2,7 @@
 #define _VARIADIC_H_
 #include <stdlib.h>
 #include <stdarg.h>
+#include <stdio.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
