7e423ddf68fa10d20d70f4ef9606c5cd0e1d1ac9
mouchang89 <mou.chang@atlasschool.com>
2024-02-26 01:07:52 +0000

    changed function

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -18,17 +18,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		if (separator != NULL)
+		printf("%d", va_arg(list, int));
+		if (separator && i < (n - 1))
 		{
-			printf("%d", va_arg(list, int));
-		}
-		else if (separator && i == 0)
-		{
-			printf("%d", va_arg(list, int));
-		}
-		else
-		{
-			printf("%s", separator, va_arg(list, int));
+			printf("%s", separator);
 		}
 	}
 	va_end(list);
