de05ffc7218f2b20ec88e75cd795f650f05989e9
mouchang89 <mou.chang@atlasschool.com>
2024-02-26 01:19:07 +0000

    fixed grammar of ; in for loop

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -17,7 +17,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(list, n);
 
-	for (i = 0, i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		str = va_arg(list, char *);
 
