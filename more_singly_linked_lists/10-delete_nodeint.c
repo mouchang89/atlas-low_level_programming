a7adf88fdaf1633ced1f4de3efcd80c6f0cbdc31
mouchang89 <mou.chang@atlasschool.com>
2024-03-11 04:27:13 +0000

    deletes node at index index of linked list

---
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes node at index index of linked list
+ * @head: pointer to list
+ * @index: where node will be deleted
+ *
+ * Return: 1 if successful, -1 if failure
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *str, *temp = *head;
+	unsigned int node;
+
+	if (temp == NULL)
+	{
+		return (-1);
+	}
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		return (1);
+	}
+	for (node = 0; node < (index - 1); node++)
+	{
+		if (temp->next == NULL)
+		{
+			return (-1);
+		}
+		temp = temp->next;
+	}
+	str = temp->next;
+	temp->next = str->next;
+	return (1);
+}
