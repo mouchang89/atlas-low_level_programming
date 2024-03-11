0325ad7b972e4ca388d9c78718261226e8e3aa3c
mouchang89 <mou.chang@atlasschool.com>
2024-03-11 22:10:13 +0000

    description too long

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * flip_bits - returns number of bits you need to flip to get from one number to another
+ * flip_bits - returns number of bits needed to get from one number to another
  * @n: first number
  * @m: second number
  *
