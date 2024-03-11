6dbaa4a3756b338a7adf31b325e83100fdcc0481
mouchang89 <mou.chang@atlasschool.com>
2024-03-11 03:48:00 +0000

    deletes head node of list

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes head node of linked list
+ * @head: pointer to head of list
+ *
+ * Return: head node's data (n) or 0 if linked list empty
+ */
+int pop_listint(listint_t **head)
+{
+	list_t *str;
+	int num;
+
+	if (!head || !*head)
+	{
+		return (0);
+	}
+	num = (*head)->n;
+	str = (*head)->next;
+	free(*head);
+	*head = str;
+	return (num);
