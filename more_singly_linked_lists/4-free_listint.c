58b645600b74312b7d57782f962770bcf9b7c6c4
mouchang89 <mou.chang@atlasschool.com>
2024-03-11 03:28:43 +0000

    frees list

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint - function that frees a listint_t list
+ * @head: pointer to head of list
+ *
+ * Return: Always 0.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *str
+	
+	while (head)
+	{
+		str = head->next;
+		free(head);
+		head = str;
+	}
+}
