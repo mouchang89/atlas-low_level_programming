3b600e3733f7b90a1c1c763164b8b127fa460203
mouchang89 <mou.chang@atlasschool.com>
2024-03-11 03:08:42 +0000

    returns number of elements in linked list

--- more_singly_linked_lists/1-listint_len.c
+++ more_singly_linked_lists/1-listint_len.c
@@ -1,4 +1,19 @@
 #include "lists.h"
 
 /**
+ * listint_len - returns number of elements in linked listint_t list
+ *  @h: pointer to head of list
  *
+ *  Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t node = 0;
+
+	while (h)
+	{
+		node++;
+		h = h->next;
+	}
+	return (node);
+}
