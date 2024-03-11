ae0ccbe8b3a927917040d68835ac35c6a7b62b7f
mouchang89 <mou.chang@atlasschool.com>
2024-03-11 03:36:06 +0000

    frees list 2

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees listint_t list
+ * @head: pointer to list
+ *
+ * Return: Always 0.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *str;
+
+	if (head == NULL);
+	{
+		return;
+	}
+	while (*head)
+	{
+		str = (*head)->next;
+		free(*head);
+		*head = str;
+	}
+	head = NULL;
+}
