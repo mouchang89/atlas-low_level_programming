23228ae92134200734bee0d65a52c07004af19b1
mouchang89 <mou.chang@atlasschool.com>
2024-03-11 01:27:53 +0000

    frees list_t

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list: frees list_t list
+ * @head: head of linked list
+ *
+ * Return: Always 0.
+ */
+void free_list(list_t *head)
+{
+	list_t *curr_node;
+
+	while ((curr_node = head) != NULL)
+	{
+		head = head->next;
+		free(curr_node->str);
+		free(curr_node);
+	}
+}
