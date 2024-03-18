a3d18c9d78a4701ab9d5aaadb0325a4e37f13b05
mouchang89 <mou.chang@atlasschool.com>
2024-03-18 22:35:56 +0000

    prints hash table

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table
+ *
+ * Return: prints hash table otherwise nothing if ht is NULL
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int i;
+	unsigned char c = 0;
+
+	if (ht == NULL)
+	{
+		return;
+	}
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (c == 1)
+		{
+			printf(", ");
+		}
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			printf("'%s': '%s'", node->key, node->value);
+			node = node->next;
+			if (node != NULL)
+			{
+				printf(", ");
+			}
+		}
+		c = 1;
+	}
+	printf("}\n");
+}
