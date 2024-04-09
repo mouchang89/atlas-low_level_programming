4da51801505377c8bb41c6f76ad7cdb8527b597d
mouchang89 <mou.chang89@atlasschool.com>
2024-04-09 16:26:26 +0000

    started binary search function

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for value in a sorted array of integers
+ * @array: pointer to first element of array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ *
+ * Return: index where value is located otherwise -1 if value not present or array is NULL
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	unsigned int i;
+
+	if (array[i] == NULL)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size, i++)
+	{
+
+	}
+	return (-1);
+}
