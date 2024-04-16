ff356be294a9ac0746e647af8eeaae1de8c61daa
mouchang89 <mou.chang@atlasscool.com>
2024-04-16 19:15:36 +0000

    moved printf statement to after for statement but before if statement, also removed 2nd printf statment before return at the end of function

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -19,12 +19,11 @@ int linear_search(int *array, size_t size, int value)
 	}
 	for (i = 0; i < size; i++)
 	{
+		printf("value checked array[%u] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 		{
-			printf("array[%d]", i);
 			return (i);
 		}
 	}
-	printf("array[%d]", i);
 	return (-1);
 }
