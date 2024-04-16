a45766c40efae621c2ad7dd97699ccccb90b59fa
mouchang89 <mou.chang@atlasscool.com>
2024-04-16 22:25:39 +0000

    added left, right, & i(mid) to function and iterated if value found at mid, on left, & on right

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -10,15 +10,32 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-	unsigned int i;
+	size_t left, right, i;
 
-	if (array[i] == NULL)
+	if (array == NULL)
 	{
 		return (-1);
 	}
-	for (i = 0; i < size, i++)
+	for (left = 0; right = size - 1; right >= left)
 	{
-
+		printf("searching in array: ");
+		for (i = left; i < right; i++)
+		{
+			printf("%d, ", array[i]);
+		}
+		printf("%d\n", array[i]);
+		i = left + (right - left) / 2;
+		if (array[i] == value)
+		{
+			return (i);
+		}
+		if (array[i] > value)
+		{
+			right = i - 1;
+		else
+		{
+			left = i + 1;
+		}
 	}
 	return (-1);
 }
