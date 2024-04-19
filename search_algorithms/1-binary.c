d9c12e7569125a31643d13609d43205670db9312
mouchang89 <mou.chang@atlasschool.com>
2024-04-19 02:48:30 +0000

    added right >= left back into first for statement

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -16,7 +16,7 @@ int binary_search(int *array, size_t size, int value)
 	{
 		return (-1);
 	}
-	for (left = 0; (right = (size - 1));)
+	for (left = 0; (right = (size - 1)); right >= left)
 	{
 		printf("searching in array: ");
 		for (i = left; i < right; i++)
